3d37efd152c88659aba7eb9f0e6d6c098621c148
hansgabriel <hansdaduya@yahoo.com>
2023-08-21 21:00:25 +0800

    First commit

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_Z3Jvd2luZy1kb2Jlcm1hbi04OC5jbGVyay5hY2NvdW50cy5kZXYk";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
