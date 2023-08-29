3f61a668d73eea1f92f2f57143041347d56836f1
3LL4N <jetdieron01@gmail.com>
2023-08-29 21:18:13 +0800

    added: Discover home section header

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Button, View } from "react-native";
 import { useAuth } from "@clerk/clerk-expo";
 import { SafeAreaView } from "react-native-safe-area-context";
+import DiscoverHomeHeader from "../components/headers/DiscoverHomeHeader";
 import MainHeader from "../components/headers/MainHeader";
 import PlayQuiz from "../components/playquiz/PlayQuiz";
 
@@ -25,6 +26,7 @@ export const HomeScreen = () => {
     <SafeAreaView className="flex-1 flex-col">
       <MainHeader />
       <PlayQuiz />
+      <DiscoverHomeHeader />
       <SignOut />
     </SafeAreaView>
   );
