918aa6009ad224be145a484144c8c4b394370eeb
hansgabriel <hansdaduya@yahoo.com>
2023-09-01 15:04:19 +0800

    Fix play card

--- apps/expo/src/components/playquiz/PlayQuiz.tsx
+++ apps/expo/src/components/playquiz/PlayQuiz.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import PlayQuizBackground from "../../icons/playquiz/PlayQuizBackground";
-import PlayQuizButton from "./PlayQuizButton";
 import type { FC } from "react";
 
 const PlayQuiz: FC = () => {
   return (
-    <View className="relative h-40 w-96">
+    <View className="mx-auto mb-3">
       <PlayQuizBackground />
-      <View className="absolute top-16 left-11 mt-16 ml-11">
-        <Text className="text-black"></Text>
-        <Text className="text-black"></Text>
-        <PlayQuizButton />
-      </View>
     </View>
   );
 };
