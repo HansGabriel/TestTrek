0dc85d51a75b044915509f1b1a6cc0a507b8d70f
3LL4N <jetdieron01@gmail.com>
2023-09-05 07:12:23 +0800

    added: prototype db query for discovery

--- apps/expo/src/screens/discover.tsx
+++ apps/expo/src/screens/discover.tsx
@@ -2,14 +2,26 @@ import React from "react";
 import { SafeAreaView, ScrollView, TouchableOpacity } from "react-native";
 import DiscoverScreenHeader from "../components/headers/DiscoverScreenHeader";
 import DiscoverScreenCard from "../components/discover/DiscoverScreenCard";
-import discoverCardList from "../temp-data/discover/discoverCardList";
+import { trpc } from "../utils/trpc";
 
 export const DiscoverScreen = () => {
+  const { data: tests } = trpc.test.getAll.useQuery();
+
+  const discoverScreenCardList: Card[] = tests.map((test) => ({
+    imageSource: test.imageUrl,
+    date: test.createdAt,
+    plays: 10,
+    q: 10,
+    title: test.title,
+    userImageSource: require("../temp-data/discover/discover-images/user-image/user-image1.png"),
+    userName: "user 1",
+  }));
+
   return (
     <SafeAreaView className="flex-1 flex-col">
       <DiscoverScreenHeader />
       <ScrollView>
-        {discoverCardList.map((card, index) => (
+        {discoverScreenCardList.map((card, index) => (
           <TouchableOpacity key={index}>
             <DiscoverScreenCard {...card} />
           </TouchableOpacity>
