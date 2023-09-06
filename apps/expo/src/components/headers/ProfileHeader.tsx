58f7b4fd9791f8f7c5aae3ef7effbfc1321ba859
3LL4N <jetdieron01@gmail.com>
2023-09-06 17:48:50 +0800

    fix: rectangle profile icon not displaying

--- apps/expo/src/components/headers/ProfileHeader.tsx
+++ apps/expo/src/components/headers/ProfileHeader.tsx
@@ -8,16 +8,14 @@ import type { FC } from "react";
 const ProfileHeader: FC = ({}) => {
   return (
     <>
-      <View className="sticky top-9 z-50 mx-6 flex flex-row justify-between bg-white py-5">
+      <View className="sticky top-9 z-50 mx-6 mb-10 flex flex-row justify-between bg-white py-5">
         <TouchableOpacity className="flex flex-row items-center gap-4">
-          {/* Added items-center here */}
           <TinyTestTrekIcon />
           <Text className="font-nunito-bold text-2xl leading-[38.40px] text-neutral-800">
             Profile
           </Text>
         </TouchableOpacity>
         <View className="flex flex-row items-center gap-4">
-          {/* Added items-center here for consistency */}
           <TouchableOpacity>
             <SettingsIcon />
           </TouchableOpacity>
