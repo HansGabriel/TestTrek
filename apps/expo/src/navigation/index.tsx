655b99d9753d1846e0ec098b9dae9b72659e2917
hansgabriel <hansdaduya@yahoo.com>
2023-09-06 23:14:37 +0800

    Add signin screen

--- apps/expo/src/navigation/index.tsx
+++ apps/expo/src/navigation/index.tsx
@@ -8,9 +8,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
 import * as React from "react";
 
-import { SignInSignUpScreen } from "../screens/signin";
+import { SigninScreen } from "../screens/signin";
 import { HomeScreen } from "../screens/home";
-import { MaterialInput } from "../screens/material-input";
 import { WaltkthroughScreen } from "../screens/walkthrough";
 import { CreateAccountScreen } from "../screens/create-account";
 import { SignupScreen } from "../screens/signup";
@@ -144,8 +143,8 @@ const RootNavigator = () => {
               }}
             />
             <Stack.Screen
-              name="SignInSignUp"
-              component={SignInSignUpScreen}
+              name="Signin"
+              component={SigninScreen}
               options={{
                 headerShown: false,
               }}
--- apps/expo/src/screens/signin.tsx
+++ apps/expo/src/screens/signin.tsx
@@ -1,26 +1,33 @@
-import React from "react";
+import { View, TouchableOpacity, Text } from "react-native";
+import LeftArrowIcon from "../icons/LeftArrowIcon";
+import SigninForm from "../forms/SigninForm";
+import useGoBack from "../hooks/useGoBack";
 
-import { View, SafeAreaView, Button, Text } from "react-native";
+import type { FC } from "react";
 
-import SignInWithOAuth from "../components/SignInWithOAuth";
-import { RootStackScreenProps } from "../types";
+export const SigninScreen: FC = () => {
+  const goBack = useGoBack();
 
-export const SignInSignUpScreen = ({
-  navigation,
-}: RootStackScreenProps<"SignInSignUp">) => {
+  const handleSubmit = () => {};
+
   return (
-    <SafeAreaView className="bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <View className="h-full w-full justify-around p-4">
-        <SignInWithOAuth />
-        <Button
-          title="Proceed to upload screen"
-          onPress={() => navigation.navigate("UploadScreen")}
-        />
-        <Button
-          title="Proceed to create reviewer"
-          onPress={() => navigation.navigate("CreateReviewer")}
-        />
+    <View className="flex-1 flex-col justify-between px-6">
+      <View>
+        <View className="my-11 flex flex-row">
+          <TouchableOpacity onPress={goBack}>
+            <LeftArrowIcon />
+          </TouchableOpacity>
+          <View className="mx-auto h-3 w-[200px]">
+            <View className="bg-primary-1 absolute left-0 top-0 h-3 w-full rounded-[100px]"></View>
+          </View>
+        </View>
+        <View className="flex flex-col items-start justify-start">
+          <Text className="font-nunito-bold self-stretch text-start text-[32px] font-bold leading-[51.20px] text-neutral-800">
+            Hello there 👋
+          </Text>
+        </View>
+        <SigninForm onSubmit={handleSubmit} />
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
