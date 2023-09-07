5a33d2da4869b71dfee1a62fcd17147f7d9d404a
Edmel John Linaugo <68092712+EdmelKun@users.noreply.github.com>
2023-09-07 14:26:16 +0800

    feature: trpc router for querying test types (#36)
    * feature: added trpc router for querying test types
    
    * fix: addressed type-safe problems

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -2,11 +2,13 @@ import { router } from "../trpc";
 import { postRouter } from "./post";
 import { authRouter } from "./auth";
 import { testRouter } from "./test";
+import { testFilterRouter } from "./testFilter";
 
 export const appRouter = router({
   post: postRouter,
   auth: authRouter,
   test: testRouter,
+  testFilter: testFilterRouter
 });
 
 // export type definition of API
---
+++ packages/api/src/router/testFilter.ts
@@ -0,0 +1,71 @@
+import { router, protectedProcedure } from "../trpc";
+import { testFiltersSchema } from "@acme/schema/src/testFilter";
+import { Prisma } from "@acme/db";
+
+export const testFilterRouter = router({
+  getAll: protectedProcedure
+    .input(testFiltersSchema)
+    .query(async ({ ctx, input }) => {
+      const { testType, sortBy } = input;
+
+      const isUser: Prisma.TestWhereInput = {
+        userId: ctx.auth.userId,
+      };
+
+      const isOther: Prisma.TestWhereInput = {
+        userId: {
+          not: ctx.auth.userId,
+        },
+      };
+
+      const favoriteTestIds = await ctx.prisma.favoriteTest
+        .findMany({
+          where: {
+            userId: ctx.auth.userId,
+          },
+          select: {
+            testId: true,
+          },
+        })
+        .then((favorites) => favorites.map((favorite) => favorite.testId));
+
+      const isFavorite: Prisma.TestWhereInput = {
+        id: {
+          in: favoriteTestIds,
+        },
+      };
+
+      console.log(isUser);
+      console.log(isOther);
+      console.log(isFavorite);
+
+      return ctx.prisma.test.findMany({
+        where:
+          testType === "user"
+            ? isUser
+            : testType === "favorite"
+            ? isFavorite
+            : isOther,
+        orderBy: {
+          createdAt: sortBy === "newest" ? "desc" : "asc",
+        },
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          imageUrl: true,
+          keywords: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          collection: true,
+          visibility: true,
+          userId: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+    }),
+});
