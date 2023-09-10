7c145d9fa49548a136e60e5f81e57a7402961b68
hansgabriel <hansdaduya@yahoo.com>
2023-09-10 15:46:56 +0800

    Add modal for time and points

--- apps/expo/src/screens/create-question.tsx
+++ apps/expo/src/screens/create-question.tsx
@@ -16,6 +16,7 @@ import { BlurView } from "expo-blur";
 import useGoBack from "../hooks/useGoBack";
 import CheckboxIcon from "../icons/CheckboxIcon";
 import TestImagePicker from "../components/ImagePicker";
+import OptionModal from "../components/modals/OptionModal";
 
 import type { FC } from "react";
 
@@ -25,6 +26,13 @@ interface Choice {
   isCorrect: boolean;
   styles: string;
 }
+
+type Option = {
+  id: string;
+  title: string;
+  value: number;
+  isSelected: boolean;
+};
 
 type ChoiceStyle = {
   styles: string;
@@ -45,12 +53,79 @@ const choiceStyles: ChoiceStyle[] = [
   },
 ];
 
+const TIME_LIMIT_OPTIONS: Option[] = [
+  {
+    id: "1",
+    title: "5 sec",
+    value: 5,
+    isSelected: false,
+  },
+  {
+    id: "2",
+    title: "10 sec",
+    value: 10,
+    isSelected: false,
+  },
+  {
+    id: "3",
+    title: "20 sec",
+    value: 20,
+    isSelected: false,
+  },
+  {
+    id: "4",
+    title: "30 sec",
+    value: 30,
+    isSelected: false,
+  },
+  {
+    id: "5",
+    title: "45 sec",
+    value: 45,
+    isSelected: false,
+  },
+  {
+    id: "6",
+    title: "60 sec",
+    value: 60,
+    isSelected: false,
+  },
+  {
+    id: "7",
+    title: "90 sec",
+    value: 90,
+    isSelected: false,
+  },
+  {
+    id: "8",
+    title: "120 sec",
+    value: 120,
+    isSelected: false,
+  },
+];
+
+const POINT_OPTIONS: Option[] = [
+  { id: "1", title: "50 pt", value: 50, isSelected: false },
+  { id: "2", title: "100 pt", value: 100, isSelected: false },
+  { id: "3", title: "200 pt", value: 200, isSelected: false },
+  { id: "4", title: "250 pt", value: 250, isSelected: false },
+  { id: "5", title: "500 pt", value: 500, isSelected: false },
+  { id: "6", title: "750 pt", value: 750, isSelected: false },
+  { id: "7", title: "1000 pt", value: 1000, isSelected: false },
+  { id: "8", title: "2000 pt", value: 2000, isSelected: false },
+];
+
 export const CreateQuestionScreen: FC = () => {
   const goBack = useGoBack();
 
+  const [timeLimitOptions, setTimeLimitOptions] =
+    useState<Option[]>(TIME_LIMIT_OPTIONS);
+  const [pointOptions, setPointOptions] = useState<Option[]>(POINT_OPTIONS);
   const [image, setImage] = useState<string | undefined>(undefined);
   const [isTextInputFocused, setIsTextInputFocused] = useState<boolean>(false);
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [showTimeLimitModal, setShowTimeLimitModal] = useState<boolean>(false);
+  const [showPointModal, setShowPointModal] = useState<boolean>(false);
   const [questionTitle, setQuestionTitle] = useState<string>("");
   const [selectedQuestionId, setSelectedQuestionId] = useState<number>(0);
   const [choices, setChoices] = useState<Choice[]>(
@@ -143,15 +218,23 @@ export const CreateQuestionScreen: FC = () => {
 
         <View className="flex flex-row items-center justify-between">
           <View className="flex flex-row gap-2">
-            <TouchableOpacity className="flex items-center justify-center rounded-[100px] bg-violet-600 px-5 py-2">
+            <TouchableOpacity
+              className="flex items-center justify-center rounded-[100px] bg-violet-600 px-5 py-2"
+              onPress={() => setShowTimeLimitModal(true)}
+            >
               <Text className="text-center text-base font-semibold leading-snug tracking-tight text-white">
-                10 sec
+                {timeLimitOptions.find((option) => option.isSelected)?.title ??
+                  "Time Limit"}
               </Text>
             </TouchableOpacity>
 
-            <TouchableOpacity className="flex items-center justify-center rounded-[100px] bg-violet-600 px-5 py-2">
+            <TouchableOpacity
+              className="flex items-center justify-center rounded-[100px] bg-violet-600 px-5 py-2"
+              onPress={() => setShowPointModal(true)}
+            >
               <Text className="text-center text-base font-semibold leading-snug tracking-tight text-white">
-                100 pt
+                {pointOptions.find((option) => option.isSelected)?.title ??
+                  "Points"}
               </Text>
             </TouchableOpacity>
           </View>
@@ -275,6 +358,22 @@ export const CreateQuestionScreen: FC = () => {
             </BlurView>
           </TouchableWithoutFeedback>
         </Modal>
+
+        <OptionModal
+          title="Time Limit"
+          options={timeLimitOptions}
+          setOptions={setTimeLimitOptions}
+          isVisible={showTimeLimitModal}
+          setIsVisible={setShowTimeLimitModal}
+        />
+
+        <OptionModal
+          title="Points"
+          options={pointOptions}
+          setOptions={setPointOptions}
+          isVisible={showPointModal}
+          setIsVisible={setShowPointModal}
+        />
 
         <View className="mt-10 flex flex-row items-center justify-between border-t border-neutral-100 bg-white px-6 pt-6 pb-9">
           <TouchableOpacity className="relative h-[58px] w-24">
