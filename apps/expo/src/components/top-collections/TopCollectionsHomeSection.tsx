665190968d9a665d07ecc1652eaf969223931ae2
3LL4N <jetdieron01@gmail.com>
2023-09-16 19:36:04 +0800

    updated: prototype trpc query in home cards

--- apps/expo/src/components/top-collections/TopCollectionsHomeSection.tsx
+++ apps/expo/src/components/top-collections/TopCollectionsHomeSection.tsx
@@ -1,21 +1,34 @@
 import { View, FlatList, TouchableOpacity } from "react-native";
 import TopCollectionsHomeHeader from "../headers/TopCollectionsHomeHeader";
 import TopCollectionsHomeCard from "./TopCollectionsHomeCard";
-import { collectionData } from "../../temp-data/library-tab-contents/collectionTestData";
 
 import type { FC } from "react";
+import { trpc } from "../../utils/trpc";
 
 const TopCollectionsHomeSection: FC = () => {
+  const { data: topCollections } = trpc.collection.getTopCollections.useQuery();
+
+  if (!topCollections) {
+    return <></>;
+  }
+
   return (
     <View>
       <TopCollectionsHomeHeader />
       <FlatList
         horizontal={true}
         showsHorizontalScrollIndicator={false}
-        data={collectionData}
+        data={topCollections.map((collection) => ({
+          id: collection.id,
+          imageSource: collection.imageUrl,
+          title: collection.title,
+        }))}
         renderItem={({ item }) => (
           <TouchableOpacity>
-            <TopCollectionsHomeCard imageSource={item.image} name={item.name} />
+            <TopCollectionsHomeCard
+              imageSource={item.imageSource}
+              title={item.title}
+            />
           </TouchableOpacity>
         )}
       />
