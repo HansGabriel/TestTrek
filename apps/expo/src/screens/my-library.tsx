7de2eeba6c24b97a1e4bbe1683312462addc92d2
EdmelKun <elinaugo137@gmail.com>
2023-09-24 01:09:43 +0800

    feature: implemented search ui in library screen

--- apps/expo/src/screens/my-library.tsx
+++ apps/expo/src/screens/my-library.tsx
@@ -1,12 +1,14 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { View, SafeAreaView, Text, TouchableOpacity } from "react-native";
 import TinyTestTrekIcon from "../icons/logos/TinyTestTrekIcon";
-import { AntDesign } from "@expo/vector-icons";
-import { Feather } from "@expo/vector-icons";
 import useGoBack from "../hooks/useGoBack";
 import { useState } from "react";
 import { HeaderAndContent } from "../components/my-library/HeaderAndContent";
 import Footer from "../components/Footer";
+import SearchIcon from "../icons/SearchIcon";
+import { SearchField } from "../components/search/SearchField";
+import Animated, { SlideInLeft, SlideOutLeft } from "react-native-reanimated";
+import LeftArrowIcon from "../icons/LeftArrowIcon";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -25,19 +27,53 @@ export const MyLibraryScreen = () => {
     return pressed ? "text-white" : "text-violet-600";
   };
 
+  const [query, setQuery] = useState("");
+  const [isSearchPressed, setIsSearchPressed] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const updateQuery = (input: string) => {
+    setQuery(input);
+  };
+
+  const handlePressed = () => {
+    setIsSearchPressed(!isSearchPressed);
+    setIsClicked(false);
+  };
+
   const goBack = useGoBack();
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <SafeAreaView className="my-10 mx-6 flex-row items-center justify-between space-x-4">
-        <View className="flex-row gap-4">
-          <TouchableOpacity onPress={goBack}>
-            <Feather name="x" size={24} color="black" />
-          </TouchableOpacity>
-          <TinyTestTrekIcon />
-          <Text className=" font-nunito-bold text-2xl">Library</Text>
-        </View>
-        <AntDesign name="search1" size={24} color="black" />
+      <SafeAreaView className="mx-6 mt-10 flex-row items-center justify-between space-x-4">
+        {isSearchPressed ? (
+          <View className="sticky z-50 flex-row justify-between self-center bg-white">
+            <SearchField
+              searchString={query}
+              onChange={updateQuery}
+              onClose={() => setIsSearchPressed(false)}
+              clicked={isClicked}
+              setClicked={() => setIsClicked(!isClicked)}
+            />
+          </View>
+        ) : (
+          <Animated.View
+            className="sticky top-0 z-50 h-full w-full flex-row justify-between bg-white pt-2 pb-3"
+            entering={SlideInLeft}
+            exiting={SlideOutLeft}
+          >
+            <View className="flex-row gap-4">
+              <TouchableOpacity onPress={goBack} className="self-center">
+                <LeftArrowIcon />
+              </TouchableOpacity>
+              <TinyTestTrekIcon />
+              <Text className=" font-nunito-bold text-2xl">Library</Text>
+            </View>
+            <TouchableOpacity onPress={handlePressed}>
+              <SearchIcon />
+            </TouchableOpacity>
+          </Animated.View>
+        )}
       </SafeAreaView>
+
       <Tab.Navigator
         className="mx-4"
         sceneContainerStyle={{
