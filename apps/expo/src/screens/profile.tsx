713a414822afe0c635602172cb590a2955e0928b
EdmelKun <elinaugo137@gmail.com>
2023-10-03 03:14:36 +0800

    fix: made profile header visible while loading

--- apps/expo/src/screens/profile.tsx
+++ apps/expo/src/screens/profile.tsx
@@ -10,11 +10,11 @@ import ProfileHeader from "../components/headers/ProfileHeader";
 import ProfileDetailsSection from "../components/profile-details/details-section/ProfileDetailsSection";
 import RectangleProfileIcon from "../icons/RectangleProfileIcon";
 import { CollectionsTab } from "../components/my-collections/Collections";
-import { TabContent } from "../components/my-library/TabContent";
 import { AboutUser } from "../components/AboutUser";
 import Footer from "../components/Footer";
 import { trpc } from "../utils/trpc";
 import { SkeletonLoader } from "../components/loaders/SkeletonLoader";
+import { LibraryTabs } from "../components/my-library/LibraryTabs";
 
 enum Tabs {
   TESTS,
@@ -44,83 +44,11 @@ export const ProfileScreen = () => {
     return activeTab === tab ? "text-white" : "text-violet-600";
   };
 
-  return (
-    <SafeAreaView className="flex-1">
-      {userData ? (
-        <View className="flex-1">
-          <ProfileHeader />
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-            stickyHeaderIndices={[1]}
-            style={{ flex: 1 }}
-          >
-            <View className="flex-1 items-center">
-              <RectangleProfileIcon width={"90%"} />
-              <ProfileDetailsSection
-                userDetails={userData}
-                testDetails={testData}
-                collectionDetails={collectionData}
-                totalPlays={totalUserPlays}
-              />
-            </View>
-
-            <View className=" h-20 w-[90%] flex-row justify-around self-center bg-white py-5">
-              <TouchableOpacity
-                onPress={() => setActiveTab(Tabs.TESTS)}
-                className={`w-[30%] items-center justify-center  rounded-[100px] border-2 border-violet-600 ${changeButtonColor(
-                  Tabs.TESTS,
-                )}`}
-              >
-                <Text
-                  className={`${changeTextColor(
-                    Tabs.TESTS,
-                  )} font-nunito-semibold text-xs`}
-                >
-                  Tests
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => setActiveTab(Tabs.COLLECTIONS)}
-                className={`w-[30%] items-center justify-center  rounded-[100px] border-2 border-violet-600 ${changeButtonColor(
-                  Tabs.COLLECTIONS,
-                )}`}
-              >
-                <Text
-                  className={`${changeTextColor(
-                    Tabs.COLLECTIONS,
-                  )} font-nunito-semibold text-xs`}
-                >
-                  Collections
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => setActiveTab(Tabs.ABOUT)}
-                className={`w-[30%] items-center justify-center  rounded-[100px] border-2 border-violet-600 ${changeButtonColor(
-                  Tabs.ABOUT,
-                )}`}
-              >
-                <Text
-                  className={`${changeTextColor(
-                    Tabs.ABOUT,
-                  )} font-nunito-semibold text-xs`}
-                >
-                  About
-                </Text>
-              </TouchableOpacity>
-            </View>
-
-            <View className="min-h-screen w-[90%] flex-1 self-center">
-              {activeTab === Tabs.TESTS && <TabContent tabData={testData} />}
-              {activeTab === Tabs.COLLECTIONS && (
-                <CollectionsTab tabData={collectionData} />
-              )}
-              {activeTab === Tabs.ABOUT && <AboutUser />}
-            </View>
-          </ScrollView>
-          <Footer />
-        </View>
-      ) : (
-        <View className="mt-20 h-[90%] w-[90%] items-center space-y-10 self-center">
+  if (!userData) {
+    return (
+      <>
+        <ProfileHeader />
+        <View className="h-[80%] w-[90%] items-center space-y-10 self-center">
           <View className=" h-[25%] w-[100%] items-center justify-center">
             <SkeletonLoader isCircular={true} width={"100%"} height={"75%"} />
           </View>
@@ -132,7 +60,84 @@ export const ProfileScreen = () => {
             <Footer />
           </View>
         </View>
-      )}
+      </>
+    );
+  }
+
+  return (
+    <SafeAreaView className="flex-1">
+      <ProfileHeader />
+      <View className="flex-1">
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          stickyHeaderIndices={[1]}
+          style={{ flex: 1 }}
+        >
+          <View className="flex-1 items-center">
+            <RectangleProfileIcon width={"90%"} />
+            <ProfileDetailsSection
+              userDetails={userData}
+              testDetails={testData}
+              collectionDetails={collectionData}
+              totalPlays={totalUserPlays}
+            />
+          </View>
+
+          <View className=" h-20 w-[90%] flex-row justify-around self-center bg-white py-5">
+            <TouchableOpacity
+              onPress={() => setActiveTab(Tabs.TESTS)}
+              className={`w-[30%] items-center justify-center  rounded-[100px] border-2 border-violet-600 ${changeButtonColor(
+                Tabs.TESTS,
+              )}`}
+            >
+              <Text
+                className={`${changeTextColor(
+                  Tabs.TESTS,
+                )} font-nunito-semibold text-xs`}
+              >
+                Tests
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setActiveTab(Tabs.COLLECTIONS)}
+              className={`w-[30%] items-center justify-center  rounded-[100px] border-2 border-violet-600 ${changeButtonColor(
+                Tabs.COLLECTIONS,
+              )}`}
+            >
+              <Text
+                className={`${changeTextColor(
+                  Tabs.COLLECTIONS,
+                )} font-nunito-semibold text-xs`}
+              >
+                Collections
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setActiveTab(Tabs.ABOUT)}
+              className={`w-[30%] items-center justify-center  rounded-[100px] border-2 border-violet-600 ${changeButtonColor(
+                Tabs.ABOUT,
+              )}`}
+            >
+              <Text
+                className={`${changeTextColor(
+                  Tabs.ABOUT,
+                )} font-nunito-semibold text-xs`}
+              >
+                About
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="min-h-screen w-[90%] flex-1 self-center">
+            {activeTab === Tabs.TESTS && <LibraryTabs tabData={testData} />}
+            {activeTab === Tabs.COLLECTIONS && (
+              <CollectionsTab tabData={collectionData} />
+            )}
+            {activeTab === Tabs.ABOUT && <AboutUser />}
+          </View>
+        </ScrollView>
+        <Footer />
+      </View>
     </SafeAreaView>
   );
 };
