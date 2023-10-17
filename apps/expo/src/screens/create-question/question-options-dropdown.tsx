b07b475929c27994c39aec28de1a5b9505095b0e
EdmelKun <elinaugo137@gmail.com>
2023-10-17 23:10:10 +0800

    chore: removed unnecessary comments

--- apps/expo/src/screens/create-question/question-options-dropdown.tsx
+++ apps/expo/src/screens/create-question/question-options-dropdown.tsx
@@ -10,7 +10,6 @@ import useToggleImageStore from "../../stores/useToggleImageStore";
 import type { FC } from "react";
 
 interface Props {
-  // onSave?: () => void;
   onDelete: () => void;
 }
 
@@ -43,18 +42,6 @@ const QuestionOptionsDropdown: FC<Props> = ({ onDelete }) => {
           <View className="relative h-[0px] w-[85px]">
             <View className="absolute left-0 top-0 h-[0px] w-[85px] border border-zinc-100"></View>
           </View>
-          {/* <TouchableOpacity
-            className="ml-4 mr-auto flex flex-row items-center justify-between"
-            onPress={onSave}
-          >
-            <SaveIcon />
-            <Text className="ml-2 text-sm font-semibold leading-tight tracking-tight text-neutral-800">
-              Save
-            </Text>
-          </TouchableOpacity> */}
-          {/* <View className="relative h-[0px] w-[85px]">
-            <View className="absolute left-0 top-0 h-[0px] w-[85px] border border-zinc-100"></View>
-          </View> */}
           <TouchableOpacity
             className="ml-4 mr-auto flex flex-row items-center justify-between"
             onPress={onDelete}
