7f3133c13e911dbc43df730c9e24735a354392bb
hansgabriel <hansdaduya@yahoo.com>
2023-11-03 22:26:40 +0800

    Fix context return type

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -11,7 +11,7 @@ import type {
  * Replace this with an object if you want to pass things to createContextInner
  */
 type AuthContextProps = {
-  auth: SignedInAuthObject | SignedOutAuthObject;
+  auth: ReturnType<typeof getAuth>;
 };
 
 /** Use this helper for:
