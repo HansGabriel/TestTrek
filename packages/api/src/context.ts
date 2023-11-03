bec74ddc491e1a58425daa05145f1ec7ada006ac
hansgabriel <hansdaduya@yahoo.com>
2023-11-03 22:26:51 +0800

    Fix context return type

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -2,10 +2,6 @@ import { prisma } from "@acme/db";
 import { type inferAsyncReturnType } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { getAuth } from "@clerk/nextjs/server";
-import type {
-  SignedInAuthObject,
-  SignedOutAuthObject,
-} from "@clerk/nextjs/api";
 
 /**
  * Replace this with an object if you want to pass things to createContextInner
