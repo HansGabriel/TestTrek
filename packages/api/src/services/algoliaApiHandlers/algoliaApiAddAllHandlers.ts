8e87b10ed9c3886922fc771f41cb12527062bb8d
JET.D <70251380+3LL4N@users.noreply.github.com>
2023-11-16 12:53:09 +0800

    added: algolia intializers (backend implementation for now) (#187) (#189)
    * added: initial configuration for tests in algolia
    
    * added: intialization for users, collections, reviewer in algolia
    
    * refactored: added script to intialize algolia
    
    * removed: algolia intializer from routers

---
+++ packages/api/src/services/algoliaApiHandlers/algoliaApiAddAllHandlers.ts
@@ -0,0 +1,123 @@
+import {
+  CollectionsForAlgolia,
+  ReviewersForAlgolia,
+  TestsForAlgolia,
+  UsersForAlgolia,
+} from "./algoliaTypes/algoliaTestsTypes";
+import algoliasearch from "algoliasearch";
+
+import "dotenv/config";
+
+const initializeAlgoliaClient = () => {
+  const applicationId = process.env.ALGOLIA_APP_ID;
+  const adminKey = process.env.ALGOLIA_ADMIN_KEY;
+
+  if (!applicationId || !adminKey) {
+    throw new Error("ALGOLIA_APP_ID and ALGOLIA_ADMIN_KEY are required");
+  }
+
+  return algoliasearch(applicationId, adminKey);
+};
+
+const parseTestsToAlgolia = async (data: Promise<TestsForAlgolia[]>) => {
+  const testsAlgoliaRecords = (await data).map(
+    ({ id, questions, ...rest }) => ({
+      objectID: id,
+      questions: questions?.length,
+      ...rest,
+    }),
+  );
+
+  return testsAlgoliaRecords;
+};
+
+export const addAllTestsToAlgolia = async (
+  queriedTests: Promise<TestsForAlgolia[]>,
+) => {
+  const data = queriedTests;
+
+  const testsAlgoliaRecords = parseTestsToAlgolia(data);
+
+  const client = initializeAlgoliaClient();
+  const testsIndex = client.initIndex("tests");
+
+  testsIndex.saveObjects(await testsAlgoliaRecords).then(() => {
+    console.log("Tests Succesfully Added to Algolia Index");
+  });
+};
+
+const parseUsersToAlgolia = async (data: Promise<UsersForAlgolia[]>) => {
+  const usersAlgoliaRecords = (await data).map(({ id, ...rest }) => ({
+    objectID: id,
+    ...rest,
+  }));
+
+  return usersAlgoliaRecords;
+};
+
+export const addAllUsersToAlgolia = async (
+  queriedUsers: Promise<UsersForAlgolia[]>,
+) => {
+  const data = queriedUsers;
+
+  const usersAlgoliaRecords = parseUsersToAlgolia(data);
+
+  const client = initializeAlgoliaClient();
+  const usersIndex = client.initIndex("users");
+
+  usersIndex.saveObjects(await usersAlgoliaRecords).then(() => {
+    console.log("Users Succesfully Added to Algolia Index");
+  });
+};
+
+const parseCollectionsToAlgolia = async (
+  data: Promise<CollectionsForAlgolia[]>,
+) => {
+  const collectionsAlgoliaRecords = (await data).map(({ id, ...rest }) => ({
+    objectID: id,
+    ...rest,
+  }));
+
+  return collectionsAlgoliaRecords;
+};
+
+export const addAllCollectionsToAlgolia = async (
+  queriedCollections: Promise<CollectionsForAlgolia[]>,
+) => {
+  const data = queriedCollections;
+
+  const collectionsAlgoliaRecords = parseCollectionsToAlgolia(data);
+
+  const client = initializeAlgoliaClient();
+  const collectionsIndex = client.initIndex("collections");
+
+  collectionsIndex.saveObjects(await collectionsAlgoliaRecords).then(() => {
+    console.log("Collections Succesfully Added to Algolia Index");
+  });
+};
+
+const parseReviewersToAlgolia = async (
+  data: Promise<ReviewersForAlgolia[]>,
+) => {
+  const reviewersAlgoliaRecords = (await data).map(({ id, ...rest }) => ({
+    objectID: id,
+    ...rest,
+  }));
+
+  return reviewersAlgoliaRecords;
+};
+
+export const addAllReviewersToAlgolia = async (
+  queriedReviewers: Promise<ReviewersForAlgolia[]>,
+) => {
+  const data = queriedReviewers;
+
+  const reviewersAlgoliaRecords = parseReviewersToAlgolia(data);
+
+  const client = initializeAlgoliaClient();
+  const reviewersIndex = client.initIndex("reviewers");
+
+  reviewersIndex.saveObjects(await reviewersAlgoliaRecords).then(() => {
+    console.log("Reviewers Succesfully Added to Algolia Index");
+  });
+};
---
+++ packages/api/src/services/algoliaApiHandlers/algoliaInitialize.ts
@@ -0,0 +1,113 @@
+import {
+  addAllCollectionsToAlgolia,
+  addAllReviewersToAlgolia,
+  addAllTestsToAlgolia,
+  addAllUsersToAlgolia,
+} from "./algoliaApiAddAllHandlers";
+import { prisma } from "@acme/db";
+
+const initializeAlgolia = async () => {
+  try {
+    console.log("Running initialization tasks...");
+    await addAllTestsToAlgolia(
+      prisma.test.findMany({
+        where: {
+          visibility: "public",
+        },
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          imageUrl: true,
+          keywords: {
+            select: {
+              name: true,
+            },
+          },
+          questions: {
+            select: {
+              id: true,
+            },
+          },
+          visibility: true,
+          user: {
+            select: {
+              userId: true,
+              imageUrl: true,
+              firstName: true,
+              lastName: true,
+            },
+          },
+          createdAt: true,
+          updatedAt: true,
+        },
+        orderBy: {
+          updatedAt: "desc",
+        },
+      }),
+    );
+
+    await addAllUsersToAlgolia(
+      prisma.user.findMany({
+        select: {
+          id: true,
+          userId: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          imageUrl: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      }),
+    );
+
+    await addAllCollectionsToAlgolia(
+      prisma.collection.findMany({
+        where: {
+          visibility: "public",
+        },
+        select: {
+          id: true,
+          user: {
+            select: {
+              userId: true,
+              imageUrl: true,
+              firstName: true,
+              lastName: true,
+            },
+          },
+          title: true,
+          imageUrl: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      }),
+    );
+
+    await addAllReviewersToAlgolia(
+      prisma.reviewer.findMany({
+        where: { visibility: "public" },
+        select: {
+          id: true,
+          title: true,
+          imageUrl: true,
+          user: {
+            select: {
+              userId: true,
+              imageUrl: true,
+              firstName: true,
+              lastName: true,
+            },
+          },
+          createdAt: true,
+          updatedAt: true,
+        },
+      }),
+    );
+  } catch (error) {
+    console.error("Error during initialization:", error);
+  }
+};
+
+initializeAlgolia();
