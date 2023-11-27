64b3a48c17250094db989d23b71d7391efde22a1
hansgabriel <hansdaduya@yahoo.com>
2023-11-28 00:01:30 +0800

    Add test history screen

--- apps/expo/src/screens/test-history/index.tsx
+++ apps/expo/src/screens/test-history/index.tsx
@@ -1,5 +1,11 @@
-import { Text, View } from "react-native";
+import { View, SafeAreaView, Dimensions } from "react-native";
 import { RootStackScreenProps } from "../../types";
+import { FlashList } from "@shopify/flash-list";
+import QuestionCard from "../../components/cards/QuestionCard";
+import { ReusableHeader } from "../../components/headers/ReusableHeader";
+import { Feather } from "@expo/vector-icons";
+import { trpc } from "../../utils/trpc";
+import { AppButton } from "../../components/buttons/AppButton";
 
 import type { FC } from "react";
 
@@ -9,7 +15,13 @@ export const TestHistoryScreen: FC<TestHistoryProps> = ({
   navigation,
   route,
 }) => {
+  const { height, width } = Dimensions.get("window");
   const { testId } = route.params;
+
+  const { data: testHistory, isLoading: isFetchingUser } =
+    trpc.testHistory.getUserHistoryById.useQuery({
+      historyId: testId,
+    });
 
   const goToScoreBoard = () => {
     navigation.navigate("Scoreboard", {
@@ -18,9 +30,41 @@ export const TestHistoryScreen: FC<TestHistoryProps> = ({
   };
 
   return (
-    <View>
-      <Text>Test History</Text>
-      <Text>{testId}</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        height: height,
+        width: width,
+      }}
+    >
+      <ReusableHeader
+        screenName="Test History"
+        backIcon={<Feather name="x" size={24} color="black" />}
+      />
+      <View className="mx-5 h-full flex-1">
+        <FlashList
+          estimatedItemSize={10}
+          data={testHistory?.questions ?? []}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item: question, index }) => {
+            return <QuestionCard question={question} index={index} />;
+          }}
+        />
+      </View>
+      <View className="mb-4 mt-10 w-[90%] flex-row items-center justify-evenly self-center">
+        <AppButton
+          text="Go to Scoreboard"
+          buttonColor="violet-600"
+          borderShadowColor="indigo-800"
+          borderRadius="full"
+          fontStyle="bold"
+          textColor="white"
+          TOwidth="full"
+          Vwidth="full"
+          Vheight="10"
+          onPress={goToScoreBoard}
+          isLoading={isFetchingUser}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
