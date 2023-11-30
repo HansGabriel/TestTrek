9657e6b738dfc99e11a54c8f6560bf6408736b7e
Edmel John Linaugo <68092712+EdmelKun@users.noreply.github.com>
2023-11-30 11:30:09 +0800

    feature: OCR capabilities (#210)
    * chore: changed text extraction into API
    
    * feature: working OCR capabilities

--- packages/api/src/router/pdfTextExtraction.ts
+++ packages/api/src/router/pdfTextExtraction.ts
@@ -1,19 +1,45 @@
 import { router, protectedProcedure } from "../trpc";
 import { z } from "zod";
-import { readPDFFile } from "../services/pdfTextExtractor";
+
+type OCRResult = {
+  text: string;
+};
 
 export const textExtractionRouter = router({
   extractText: protectedProcedure
     .input(
       z.object({
         file: z.string(),
+        fileType: z.string(),
       }),
     )
     .mutation(async ({ input }) => {
-      const { file } = input;
+      const { file, fileType } = input;
 
-      const content = await readPDFFile(file);
+      const apiKey = process.env.OCR_API;
+      const apiEndpoint = "https://api.ocr.space/parse/image";
 
-      return content;
+      const formData = new FormData();
+      formData.append("base64image", file);
+      formData.append("filetype", fileType);
+      if (apiKey) {
+        formData.append("apikey", apiKey);
+      }
+
+      const response = await fetch(apiEndpoint, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("OCR request failed");
+      }
+
+      const data = await response.json();
+      const ocrResult: OCRResult = {
+        text: data.ParsedResults?.[0]?.ParsedText || "",
+      };
+
+      return ocrResult;
     }),
 });
