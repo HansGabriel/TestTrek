1a07fdd612cdb343da06f68bf95358fc1608a999
JET.D <70251380+3LL4N@users.noreply.github.com>
2023-12-05 17:13:13 +0800

    added: intial tests for random question function handlers (#224)

---
+++ packages/api/src/functions/tests/randomQuestionHandlers.test.ts
@@ -0,0 +1,58 @@
+import { describe, it, expect } from "vitest";
+import {
+  questionFormatGenerators,
+  generateCombinedQuestionPrompts,
+  processGeneratedQuestions,
+} from "../randomQuestionsHandlers";
+
+import { generateChoicesPrompt, timeAndPointsPrompt } from "../gptHandlers";
+
+describe("questionFormatGenerators", () => {
+  it("It should generate format for multiple choice question/s", () => {
+    const mcqFormat = questionFormatGenerators["multipleChoice"];
+    const output = `separator\nQuestion: [Your question here, max 100 characters]
+  ${generateChoicesPrompt(
+    4,
+  )}\nCorrect Answer: Option [Correct option number] ${timeAndPointsPrompt}`;
+
+    expect(mcqFormat()).toEqual(output);
+  });
+
+  it("It should generate format for multiselect question/s", () => {
+    const multiselectFormat = questionFormatGenerators["multiselect"];
+
+    const output = `separator\nQuestion: [Your question here, max 100 characters]
+  ${generateChoicesPrompt(
+    4,
+  )}\nAll Correct Answers: Options [Correct option numbers separated by commas, e.g., 1,3] ${timeAndPointsPrompt}`;
+
+    expect(multiselectFormat()).toEqual(output);
+  });
+
+  it("It should generate format for identification question/s", () => {
+    const identicationFormat = questionFormatGenerators["identification"];
+
+    const output = `separator\nQuestion: [Your question here, max 100 characters]\nAnswer: [Your answer here, max 68 characters] ${timeAndPointsPrompt}`;
+
+    expect(identicationFormat()).toEqual(output);
+  });
+
+  it("It should generate format for true or false question/s", () => {
+    const tofFormat = questionFormatGenerators["trueOrFalse"];
+
+    const output = `separator\nQuestion: [Your question here, max 100 characters]\nAnswer: [True/False] ${timeAndPointsPrompt}`;
+
+    expect(tofFormat()).toEqual(output);
+  });
+});
+
+describe("generateCombinedQuestionPrompts", () => {
+  it("should generate correct prompt for a single question", () => {
+    const combinedPrompts = generateCombinedQuestionPrompts(
+      1,
+      "Sample message",
+    );
+    expect(combinedPrompts).toContain("Sample message");
+    expect(combinedPrompts).toContain("separator");
+  });
+});
