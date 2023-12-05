ceb7d769f7c978fdaf90ebc68eabec77e3d37ae5
Edmel John Linaugo <68092712+EdmelKun@users.noreply.github.com>
2023-12-05 13:41:06 +0800

    fix: move save question button on header

--- apps/expo/src/screens/create-question/question-options-dropdown.tsx
+++ apps/expo/src/screens/create-question/question-options-dropdown.tsx
@@ -7,11 +7,15 @@ import type { FC } from "react";
 interface Props {
   isDeleting: boolean;
   setOpenDeleteAlert: () => void;
+  isSaved: boolean;
+  handleSaveAnswer: () => void;
 }
 
 const QuestionOptionsDropdown: FC<Props> = ({
   isDeleting,
   setOpenDeleteAlert,
+  isSaved,
+  handleSaveAnswer,
 }) => {
   return (
     <View className="relative flex-row space-x-4">
@@ -22,7 +26,7 @@ const QuestionOptionsDropdown: FC<Props> = ({
           <AntDesign name="delete" size={24} color="red" />
         )}
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity disabled={isSaved} onPress={handleSaveAnswer}>
         <Entypo name="save" size={25} color="rgb(79 70 229)" />
       </TouchableOpacity>
     </View>
