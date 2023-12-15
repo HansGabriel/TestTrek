7a7b6bc670357d8c98ce66e2358d1f4138f76b96
JET.D <70251380+3LL4N@users.noreply.github.com>
2023-12-15 22:57:27 +0800

    refactor: turning premium status on and off (#273) (#274)
    * refactor: turning premium status on and off (#273)
    
    * Update index.tsx

--- apps/expo/src/screens/settings/index.tsx
+++ apps/expo/src/screens/settings/index.tsx
@@ -4,17 +4,55 @@ import { Dimensions, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SettingsHeader from "../../components/headers/SettingsHeader";
 import SettingsButtons from "../../components/settings/SettingsButtons";
-import PremiumBanner from "../../components/settings/PremiumBanner";
+import GoPremiumBanner from "../../icons/settings/GoPremiumBanner";
+import CancelPremiumBanner from "../../icons/settings/CancelPremiumBanner";
+import { AlertModal } from "../../components/modals/AlertModal";
 import { LogoutModal } from "./LogoutModal";
 import { useNavigation } from "@react-navigation/native";
+import {
+  successToast,
+  errorToast,
+} from "../../components/notifications/ToastNotifications";
+import { trpc } from "../../utils/trpc";
 
 export const SettingsScreen = () => {
   const navigation = useNavigation();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
   const bottomSheetRef = useRef<BottomSheet>(null);
 
   const snapPoints = ["25%", "75%"];
+
+  const trpcUtils = trpc.useContext();
+
+  const { data: premiumStatus, isLoading: isGettingPremiumStatus } =
+    trpc.user.getUserPremiumStatus.useQuery();
+
+  const { mutate: togglePremiumStatus, isLoading: isTogglingPremiumStatus } =
+    trpc.user.toggleUserPremiumStatus.useMutation({
+      onSuccess: () => {
+        trpcUtils.user.getUserPremiumStatus.invalidate();
+        setIsAlertModalOpen(false);
+        successToast({
+          title: "Success",
+          message: !premiumStatus
+            ? "You are now a premium user"
+            : "You are no longer a premium user",
+        });
+      },
+      onError: (err) => {
+        errorToast({
+          title: "Error",
+          message: err.message,
+        });
+        setIsAlertModalOpen(false);
+      },
+    });
+
+  const handleTogglePremiumStatus = () => {
+    togglePremiumStatus();
+  };
 
   const openBottomSheet = () => {
     bottomSheetRef.current?.expand();
@@ -47,7 +85,11 @@ export const SettingsScreen = () => {
         pointerEvents={`${isModalOpen ? "none" : "auto"}`}
       >
         <SettingsHeader screenName={"Settings"} />
-        <PremiumBanner onPress={goToPremiumScreen} />
+        {!premiumStatus ? (
+          <GoPremiumBanner onPress={goToPremiumScreen} />
+        ) : (
+          <CancelPremiumBanner onPress={() => setIsAlertModalOpen(true)} />
+        )}
         <SettingsButtons openBottomSheet={openBottomSheet} />
       </View>
       {isModalOpen && (
@@ -59,6 +101,17 @@ export const SettingsScreen = () => {
         snapPoints={snapPoints}
         onChange={handleSheetChanges}
         onClose={() => setIsModalOpen(false)}
+      />
+      <AlertModal
+        isVisible={isAlertModalOpen}
+        alertTitle="Are You Sure?"
+        alertDescription="This will cancel your premium status and you will lose premium benefits"
+        isCancelButtonVisible={true}
+        confirmButtonText="Yes"
+        cancelButtonText="No"
+        onConfirm={handleTogglePremiumStatus}
+        isLoading={isTogglingPremiumStatus || isGettingPremiumStatus}
+        onCancel={() => setIsAlertModalOpen(false)}
       />
     </SafeAreaView>
   );
